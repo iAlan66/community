35d476b0ddc5e58b0b508c153c6fa974501b8fc6
Alan <cxy814941789@gmail.com>
2022-09-27 21:42:37 +0800

    add captcha

--- src/main/java/com/alan/community/controller/LoginController.java
+++ src/main/java/com/alan/community/controller/LoginController.java
@@ -3,7 +3,9 @@ package com.alan.community.controller;
 import com.alan.community.entity.User;
 import com.alan.community.service.UserService;
 import com.alan.community.util.CommunityConstant;
-import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 /**
@@ -21,41 +30,49 @@ import java.util.Map;
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private Producer kaptcherProdeucer;
+
     /**
      * 跳转注册页面
+     *
      * @return
      */
-    @RequestMapping(value = "/register",method = RequestMethod.GET)
-    public String getRegisterPage(){
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
         return "/site/register";
     }
 
     /**
      * 跳转登录页面
+     *
      * @return
      */
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
-    public String getLoginPage(){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
         return "/site/login";
     }
 
     /**
      * 提交注册表单
+     *
      * @param model
      * @param user
      * @return
      */
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public String register(Model model, User user){
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
         Map<String, Object> map = userService.register(user);
-        if (map == null || map.isEmpty()){
-            model.addAttribute("msg","注册成功，我们已经向您的邮箱发送了一封激活邮件");
-            model.addAttribute("target","/index");
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件");
+            model.addAttribute("target", "/index");
             return "/site/operate-result";
-        }else{
+        } else {
             model.addAttribute("usernameMsg", map.get("usernameMsg"));
             model.addAttribute("passwordMsg", map.get("passwordMsg"));
             model.addAttribute("emailMsg", map.get("emailMsg"));
@@ -66,27 +83,49 @@ public class LoginController implements CommunityConstant {
 
     /**
      * 激活链接跳转
+     *
      * @param model
      * @param userId
      * @param code
      * @return
      */
     //http://localhost:8080/community/activation/101/code
-    @RequestMapping(path = "/activation/{userId}/{code}" ,method = RequestMethod.GET)
-    public String activation(Model model, @PathVariable("userId") int userId,@PathVariable("code") String code){
+    @RequestMapping(path = "/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
         int result = userService.activation(userId, code);
-        if (result == ACTIVATION_SUCCESS){
-            model.addAttribute("msg","激活成功，帐号可以使用");
-            model.addAttribute("target","/login");
-        }else if(result == ACTIVATION_REPEAT){
-            model.addAttribute("msg","无效操作");
-            model.addAttribute("target","/index");
+        if (result == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "激活成功，帐号可以使用");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作");
+            model.addAttribute("target", "/index");
 
-        }else {
-            model.addAttribute("msg","激活失败，激活码不正确");
-            model.addAttribute("target","/index");
+        } else {
+            model.addAttribute("msg", "激活失败，激活码不正确");
+            model.addAttribute("target", "/index");
 
         }
         return "/site/operate-result";
+    }
+
+    // 生成验证码
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcher(HttpServletResponse response, HttpSession session) {
+        //生成验证码
+        String text = kaptcherProdeucer.createText();
+        BufferedImage image = kaptcherProdeucer.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha",text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败："+e.getMessage());
+        }
+
     }
 }
